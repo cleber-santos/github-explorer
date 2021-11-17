8c45e821a9f72965231ac135c517e374ea380bf1
CleberSantos <cleber7284@gmail.com>
2021-11-17 09:46:12 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 import './style/global.scss'
 
 export function App() {
-  return <RepositoryList />
+  return (
+    // a tag sem nada dentro <> no react significa que é um fragmento (frag), na hora de renderizar ele fica direto os elementos em tela.
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
