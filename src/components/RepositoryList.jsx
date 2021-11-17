653f231cc089e7c13f6ba69e587117f37c4a6fcb
CleberSantos <cleber7284@gmail.com>
2021-11-17 08:43:58 -0300

    props in react

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,31 +1,20 @@
-const repositoryName = 'unform2'
+import { RepositoryItem } from "./RepositoryItem"
+
+const repository ={
+  name: 'unform',
+  description: 'Forms in react',
+  link: 'https://github.com/unform/unform'
+}
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositório</h1>
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Form in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Form in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Form in React</p>
-          <a href="">
-            Acessar repositórios
-          </a>
-        </li>
+        <RepositoryItem repository={repository}/>       
+        <RepositoryItem repository={repository}/>       
+        <RepositoryItem repository={repository}/>       
+        <RepositoryItem repository={repository}/>       
       </ul>
     </section>
   )
